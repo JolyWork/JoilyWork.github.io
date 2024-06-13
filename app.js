65c95f56b9b199fce854b9f43e6da0cdf1de900c
JolyWork <95842654+JolyWork@users.noreply.github.com>
2024-06-13 18:39:07 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-let counterValue = 0;
-
-        function toggleCounters() {
-            document.getElementById('buyButton').style.display = 'none';
-            document.getElementById('counters').style.display = 'block';
+        function toggleCounters(button) {
+            button.style.display = 'none';
+			let counters = button.nextElementSibling;
+			let counterValueElement = counters.querySelector('.counterValue');
+			counterValueElement.textContent = '1';
+            counters.style.display = 'inline';
         }
 
-        function increment() {
+       function increment(button) {
+            let counterValueElement = button.previousElementSibling;
+            let counterValue = parseInt(counterValueElement.textContent);
             counterValue++;
-            document.getElementById('counterValue').textContent = counterValue;
+            counterValueElement.textContent = counterValue;
         }
-
-        function decrement() {
+		
+        function decrement(button) {
+            let counterValueElement = button.nextElementSibling;
+            let counterValue = parseInt(counterValueElement.textContent);
             if (counterValue > 0) {
                 counterValue--;
-                document.getElementById('counterValue').textContent = counterValue;
+                counterValueElement.textContent = counterValue;
             }
+			
+			if (counterValue === 0) {
+				button.parentElement.style.display = 'none';
+				let container = button.parentElement.parentElement;
+				let buyButton = container.querySelector('.btn');
+				buyButton.style.display = 'inline';
+			}
         }
