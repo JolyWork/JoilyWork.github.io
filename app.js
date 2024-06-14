0531281088e1a98b67effd7619db6e007ac94c99
JolyWork <95842654+JolyWork@users.noreply.github.com>
2024-06-14 21:43:07 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
-        function toggleCounters(button) {
+var totalPriceFinal = 0;  
+var tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#2cab37";
+
+
+      
+		function totalPricePlus(value,price){
+			totalPriceFinal = (parseFloat(totalPriceFinal) + price).toFixed(2);
+			document.getElementById('totalPrice').textContent = "Total Price: " + totalPriceFinal;
+		}
+		
+		function totalPriceMinus(value,price){
+			totalPriceFinal = (parseFloat(totalPriceFinal) - price).toFixed(2);
+			document.getElementById('totalPrice').textContent = "Total Price: " + totalPriceFinal;
+		}
+		
+		function toggleCounters(button) {
             button.style.display = 'none';
 			let counters = button.nextElementSibling;
 			let counterValueElement = counters.querySelector('.counterValue');
 			counterValueElement.textContent = '1';
             counters.style.display = 'inline';
+			console.log(button.parentNode.querySelector('.price'));
+			var priceElement = button.parentNode.querySelector('.price');
+			let price = parseFloat(priceElement.textContent);
+			let value = parseInt(counterValueElement.textContent);
+			totalPricePlus(value, price);
+			
+			if (tg.MainButton.isVisible){
+				tg.MainButton.hide();
+			}
+			else {
+				tg.MainButton.setText(totalPriceFinal);
+			}
         }
 
        function increment(button) {
@@ -11,6 +43,10 @@
             let counterValue = parseInt(counterValueElement.textContent);
             counterValue++;
             counterValueElement.textContent = counterValue;
+			let priceElement = (button.parentNode).parentNode.querySelector('.price');
+			let price = parseFloat(priceElement.textContent);
+			totalPricePlus(counterValue,price);
+			tg.MainButton.setText(totalPriceFinal);
         }
 		
         function decrement(button) {
@@ -26,5 +62,26 @@
 				let container = button.parentElement.parentElement;
 				let buyButton = container.querySelector('.btn');
 				buyButton.style.display = 'inline';
+				
+				let allCouterValues = document.querySelectorAll('.counterValue');
+				let totalItems = Array.from(allCounterValues)
+					.map(element => parseInt(element.textContent))
+					.reduce((total, value) => total + value, 0);
+				if (totalItems === 0) {
+					tg.MainButton.setText("");
+					tg.MainButton.hide();
+				}
 			}
+			
+			let priceElement = (button.parentNode).parentNode.querySelector('.price');
+			let price = parseFloat(priceElement.textContent);
+			totalPriceMinus(counterValue, price);
         }
+		
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+	tg.sendData(totalPriceFinal);
+});
+		
+		
+		
+		
